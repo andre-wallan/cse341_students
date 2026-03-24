3eda0d9ace25eb4db83b4236e7fa9dcd1d2a3ab1
andre-wallan <andressemanda@gmail.com>
2026-03-24 16:55:43 +0300

    mgh

--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/studentsController');
+const controller = require('../controllers/studentscontroller');
 const auth = require('../middleware/auth');
 
 /**
